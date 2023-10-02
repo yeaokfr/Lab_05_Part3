cd04ecff2323bdb74330c10b37c414cd5938bcb1
yeaokfr <144458128+yeaokfr@users.noreply.github.com>
2023-10-01 20:48:50 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your party affiliation? (Respond with D,R, or I)");
+        String affiliation = scan.nextLine();
+        if (affiliation.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        } else if (affiliation.equalsIgnoreCase("R")) {
+            System.out.println("You get a Republican Elephant.");
+        } else if (affiliation.equalsIgnoreCase("I"))
+            System.out.println("You get an Independent Man");
+        else
+            System.out.println("You have entered an invalid affiliation");
     }
 }
